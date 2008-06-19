1bd4f7d583bb89f48900516e7cbab31fd0b34aff
jamiroconca <conca.daniele@gmail.com>
2008-06-19 16:27:58 +0000

    code tidying

--- pnjavascript/crpvideo.js
+++ pnjavascript/crpvideo.js
@@ -1,10 +1,9 @@
 /**
  * crpVideo
  *
- * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpcalendar Support and documentation
- * @version $Id: $ 
- * @author Daniele Conca <conca dot daniele at gmail dot com>
+ * @copyright (c) 2007-2008, Daniele Conca
+ * @link http://code.zikula.org/projects/crpvideo Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
  * @license GNU/GPL - v.2.1
  * @package crpVideo
  */
