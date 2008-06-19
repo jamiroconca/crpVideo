8f6000edce2d3cbe0cbaef04a6e3952461ff5301
jamiroconca <conca.daniele@gmail.com>
2008-06-19 16:30:30 +0000

    config upgrade

--- modules/crpVideo/pnclass/crpVideo.php
+++ modules/crpVideo/pnclass/crpVideo.php
@@ -1,17 +1,17 @@
 <?php
+
 /**
  * crpVideo
  *
- * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpvideo Support and documentation
- * @version $Id: $ 
- * @author Daniele Conca <jami at cremonapalloza dot org>
- * @license GNU/GPL - v.2
+ * @copyright (c) 2007-2008, Daniele Conca
+ * @link http://code.zikula.org/projects/crpvideo Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
  * @package crpVideo
  */
 
-Loader::includeOnce('modules/crpVideo/pnclass/crpVideoUI.php');
-Loader::includeOnce('modules/crpVideo/pnclass/crpVideoDAO.php');
+Loader :: includeOnce('modules/crpVideo/pnclass/crpVideoUI.php');
+Loader :: includeOnce('modules/crpVideo/pnclass/crpVideoDAO.php');
 
 /**
  * crpVideo Object
@@ -23,10 +23,10 @@ class crpVideo
 	{
 		$this->ui = new crpVideoUI();
 		$this->dao = new crpVideoDAO();
-		
-		(function_exists('gd_info'))?$this->gd = gd_info():$this->gd = array();
+
+		(function_exists('gd_info')) ? $this->gd = gd_info() : $this->gd = array ();
 	}
-	
+
 	/**
 	 * update a cover
 	 * 
@@ -35,20 +35,21 @@ class crpVideo
 	 * 
 	 * @return string html
 	 */
-	function createCover($video=array())
-	{	
+	function createCover($video = array ())
+	{
 		// Argument check
-		if(!$this->dao->validateData($video))
+		if (!$this->dao->validateData($video))
 			return false;
-		
+
 		$video['image']['videoid'] = $video['videoid'];
 		$video['image']['document_type'] = 'image';
 		$id_image = $this->dao->setFile($video['image']);
-		if($id_image == '-1')	return false;
-		
+		if ($id_image == '-1')
+			return false;
+
 		return true;
 	}
-	
+
 	/**
 	 * Change item status
 	 * 
@@ -59,23 +60,23 @@ class crpVideo
 	 */
 	function changeStatus()
 	{
-		$videoid = FormUtil::getPassedValue('videoid', null);
+		$videoid = FormUtil :: getPassedValue('videoid', null);
 		$obj_status = FormUtil :: getPassedValue('obj_status', null);
 
-		if ($obj_status=='P' || $obj_status=='A')
+		if ($obj_status == 'P' || $obj_status == 'A')
 		{
-			($obj_status=='A')?$obj_status='P':$obj_status='A';
-			if (!$this->dao->updateStatus($videoid, $obj_status)) 
-	      LogUtil::registerError (_UPDATEFAILED);
-	    else
-				LogUtil::registerStatus (_UPDATESUCCEDED);
+			($obj_status == 'A') ? $obj_status = 'P' : $obj_status = 'A';
+			if (!$this->dao->updateStatus($videoid, $obj_status))
+				LogUtil :: registerError(_UPDATEFAILED);
+			else
+				LogUtil :: registerStatus(_UPDATESUCCEDED);
 		}
 		else
-			LogUtil::registerError (_UPDATEFAILED);
-		
-    return pnRedirect(pnModURL('crpVideo', 'admin', 'view')); 
+			LogUtil :: registerError(_UPDATEFAILED);
+
+		return pnRedirect(pnModURL('crpVideo', 'admin', 'view'));
 	}
-	
+
 	/**
 	 * Modify module's configuration
 	 */
@@ -83,10 +84,10 @@ class crpVideo
 	{
 		// get all module vars
 		$modvars = pnModGetVar('crpVideo');
-		
+
 		return $this->ui->modifyConfig($modvars, $this->gd);
 	}
-	
+
 	/**
 	 * Update module's configuration
 	 */
@@ -97,56 +98,58 @@ class crpVideo
 		{
 			return LogUtil :: registerAuthidError(pnModURL('crpVideo', 'admin', 'view'));
 		}
-	
+
 		// Update module variables
-		$itemsperpage= (int) FormUtil :: getPassedValue('itemsperpage', 25, 'POST');
+		$itemsperpage = (int) FormUtil :: getPassedValue('itemsperpage', 25, 'POST');
 		if ($itemsperpage < 1)
 		{
-			$itemsperpage= 25;
+			$itemsperpage = 25;
 		}
 		pnModSetVar('crpVideo', 'itemsperpage', $itemsperpage);
-		$enablecategorization= (bool) FormUtil :: getPassedValue('enablecategorization', false, 'POST');
+		$enablecategorization = (bool) FormUtil :: getPassedValue('enablecategorization', false, 'POST');
 		pnModSetVar('crpVideo', 'enablecategorization', $enablecategorization);
-		$addcategorytitletopermalink= (bool) FormUtil :: getPassedValue('addcategorytitletopermalink', false, 'POST');
+		$addcategorytitletopermalink = (bool) FormUtil :: getPassedValue('addcategorytitletopermalink', false, 'POST');
 		pnModSetVar('crpVideo', 'addcategorytitletopermalink', $addcategorytitletopermalink);
-		$cover_dimension = (int)FormUtil::getPassedValue('cover_dimension', 35000, 'POST');
-	  pnModSetVar('crpVideo', 'cover_dimension', $cover_dimension);
-	  $image_width = (int)FormUtil::getPassedValue('image_width', 300, 'POST');
-	  pnModSetVar('crpVideo', 'image_width', $image_width);
-	  $playerwidth = (int)FormUtil::getPassedValue('playerwidth', 400, 'POST');
-	  pnModSetVar('crpVideo', 'playerwidth', $playerwidth);
-	  $playerheight = (int)FormUtil::getPassedValue('playerheight', 340, 'POST');
-	  pnModSetVar('crpVideo', 'playerheight', $playerheight);
-	 	$displayheight = (int)FormUtil::getPassedValue('displayheight', 300, 'POST');
-	  pnModSetVar('crpVideo', 'displayheight', $displayheight);
-	 	$file_dimension = (int)FormUtil::getPassedValue('file_dimension', 1500000, 'POST');
-	  pnModSetVar('crpVideo', 'file_dimension', $file_dimension);
-	  $upload_path = FormUtil::getPassedValue('upload_path', null, 'POST');
-	  pnModSetVar('crpVideo', 'upload_path', $upload_path);
-	  $browser_path = FormUtil::getPassedValue('browser_path', null, 'POST');
-	  pnModSetVar('crpVideo', 'browser_path', $browser_path);
-	  $crpvideo_use_gd = (bool)FormUtil::getPassedValue('crpvideo_use_gd', false, 'POST');
-	  pnModSetVar('crpVideo', 'crpvideo_use_gd', $crpvideo_use_gd);
-	  $crpvideo_userlist_image = (bool)FormUtil::getPassedValue('crpvideo_userlist_image', false, 'POST');
-	  pnModSetVar('crpVideo', 'crpvideo_userlist_image', $crpvideo_userlist_image);
-	  $userlist_width = (int)FormUtil::getPassedValue('userlist_width', 32, 'POST');
-	  pnModSetVar('crpVideo', 'userlist_width', $userlist_width);
-	  $display_embed = (bool)FormUtil::getPassedValue('display_embed', false, 'POST');
-	  pnModSetVar('crpVideo', 'display_embed', $display_embed);
-	  $mandatory_cover = (bool)FormUtil::getPassedValue('mandatory_cover', false, 'POST');
-	  pnModSetVar('crpVideo', 'mandatory_cover', $mandatory_cover);
-	  
+		$cover_dimension = (int) FormUtil :: getPassedValue('cover_dimension', 35000, 'POST');
+		pnModSetVar('crpVideo', 'cover_dimension', $cover_dimension);
+		$image_width = (int) FormUtil :: getPassedValue('image_width', 300, 'POST');
+		pnModSetVar('crpVideo', 'image_width', $image_width);
+		$playerwidth = (int) FormUtil :: getPassedValue('playerwidth', 400, 'POST');
+		pnModSetVar('crpVideo', 'playerwidth', $playerwidth);
+		$playerheight = (int) FormUtil :: getPassedValue('playerheight', 340, 'POST');
+		pnModSetVar('crpVideo', 'playerheight', $playerheight);
+		$displayheight = (int) FormUtil :: getPassedValue('displayheight', 300, 'POST');
+		pnModSetVar('crpVideo', 'displayheight', $displayheight);
+		$file_dimension = (int) FormUtil :: getPassedValue('file_dimension', 1500000, 'POST');
+		pnModSetVar('crpVideo', 'file_dimension', $file_dimension);
+		$upload_path = FormUtil :: getPassedValue('upload_path', null, 'POST');
+		pnModSetVar('crpVideo', 'upload_path', $upload_path);
+		$browser_path = FormUtil :: getPassedValue('browser_path', null, 'POST');
+		pnModSetVar('crpVideo', 'browser_path', $browser_path);
+		$crpvideo_use_gd = (bool) FormUtil :: getPassedValue('crpvideo_use_gd', false, 'POST');
+		pnModSetVar('crpVideo', 'crpvideo_use_gd', $crpvideo_use_gd);
+		$crpvideo_userlist_image = (bool) FormUtil :: getPassedValue('crpvideo_userlist_image', false, 'POST');
+		pnModSetVar('crpVideo', 'crpvideo_userlist_image', $crpvideo_userlist_image);
+		$userlist_width = (int) FormUtil :: getPassedValue('userlist_width', 32, 'POST');
+		pnModSetVar('crpVideo', 'userlist_width', $userlist_width);
+		$display_embed = (bool) FormUtil :: getPassedValue('display_embed', false, 'POST');
+		pnModSetVar('crpVideo', 'display_embed', $display_embed);
+		$mandatory_cover = (bool) FormUtil :: getPassedValue('mandatory_cover', false, 'POST');
+		pnModSetVar('crpVideo', 'mandatory_cover', $mandatory_cover);
+		$main_items = (int) FormUtil :: getPassedValue('main_items', 3, 'POST');
+		pnModSetVar('crpVideo', 'main_items', $main_items);
+
 		// Let any other modules know that the modules configuration has been updated
 		pnModCallHooks('module', 'updateconfig', 'crpVideo', array (
 			'module' => 'crpVideo'
 		));
-	
+
 		// the module configuration has been updated successfuly
 		LogUtil :: registerStatus(_CONFIGUPDATED);
-	
+
 		return pnRedirect(pnModURL('crpVideo', 'admin', 'view'));
 	}
-	
+
 	/**
 	 * Generate thumbnail for image
 	 * 
@@ -156,44 +159,45 @@ class crpVideo
 	 */
 	function getThumbnail()
 	{
-		$videoid = FormUtil::getPassedValue('videoid', isset($args['videoid']) ? $args['videoid'] : null, 'REQUEST');
-		$width = FormUtil::getPassedValue('width', isset($args['width']) ? $args['width'] : null, 'REQUEST');
-		if(!SecurityUtil::checkPermission('crpVideo::', '::', ACCESS_READ)) pnShutDown();
+		$videoid = FormUtil :: getPassedValue('videoid', isset ($args['videoid']) ? $args['videoid'] : null, 'REQUEST');
+		$width = FormUtil :: getPassedValue('width', isset ($args['width']) ? $args['width'] : null, 'REQUEST');
+		if (!SecurityUtil :: checkPermission('crpVideo::', '::', ACCESS_READ))
+			pnShutDown();
 
 		$file = $this->dao->getFile($videoid, 'image', true);
-		$modifiedDate = $this->dao->getVideoDate($videoid,'lu_date');
+		$modifiedDate = $this->dao->getVideoDate($videoid, 'lu_date');
 
-		if(!(is_numeric($width) && $width>0))
+		if (!(is_numeric($width) && $width > 0))
 			$width = pnModGetVar('crpVideo', 'image_width');
-		$params['width'] = $width;//	$params['append_ghosted']=1;
+		$params['width'] = $width; //	$params['append_ghosted']=1;
 		$params['modifiedDate'] = $modifiedDate;
 
-		crpVideo::imageGetThumbnail($file['binary_data'], $file['filename'],  $file['content_type'], $params);
+		crpVideo :: imageGetThumbnail($file['binary_data'], $file['filename'], $file['content_type'], $params);
 	}
-	
-	function imageGetThumbnail(& $pSrcImage, $filename, $content_type, $params = array())
+
+	function imageGetThumbnail(& $pSrcImage, $filename, $content_type, $params = array ())
 	{
-		$alphaThreshold = isset($params['alpha_threshold']) ? $params['alpha_threshold'] : 64;
-		$newWidth = isset($params['width']) ? $params['width'] : 100;
+		$alphaThreshold = isset ($params['alpha_threshold']) ? $params['alpha_threshold'] : 64;
+		$newWidth = isset ($params['width']) ? $params['width'] : 100;
 		$appendGhosted = $params['append_ghosted'];
 		//
 		$srcImage = imagecreatefromstring($pSrcImage);
-	
+
 		if ($srcImage)
 		{
 			//obtain the original image Height and Width
 			$srcWidth = imagesx($srcImage);
 			$srcHeight = imagesy($srcImage);
-	
+
 			$destWidth = round($newWidth, '0');
 			$destHeight = round(($srcHeight / $srcWidth) * $newWidth, '0');
-	
+
 			// creating the destination image with the new Width and Height
-			if(!$appendGhosted)
+			if (!$appendGhosted)
 				$destImage = imagecreatetruecolor($destWidth, $destHeight);
 			else
-				$destImage = imagecreatetruecolor($destWidth, 2*$destHeight);
-	
+				$destImage = imagecreatetruecolor($destWidth, 2 * $destHeight);
+
 			//png transparency
 			switch ($content_type)
 			{
@@ -202,74 +206,73 @@ class crpVideo
 					imagealphablending($destImage, false);
 					imagesavealpha($destImage, true);
 					$transparent = imagecolorallocatealpha($destImage, 255, 255, 255, 80);
-					imagefill($destImage, 0,0,$transparent);
+					imagefill($destImage, 0, 0, $transparent);
 					break;
-	
+
 				case 'image/gif' :
 					imageantialias($destImage, true);
 					imagealphablending($destImage, false);
 					break;
 			}
-	
+
 			//copy the srcImage to the destImage
 			imagecopyresampled($destImage, $srcImage, 0, 0, 0, 0, $destWidth, $destHeight, $srcWidth, $srcHeight);
-	
+
 			//
-			if($appendGhosted)
+			if ($appendGhosted)
 			{
 				imagecopyresampled($destImage, $srcImage, 0, $destHeight, 0, 0, $destWidth, $destHeight, $srcWidth, $srcHeight);
-	
+
 				$ghostImage = imagecreatetruecolor($destWidth, $destHeight);
 				imageantialias($ghostImage, true);
 				imagealphablending($ghostImage, false);
 				imagesavealpha($ghostImage, true);
 				$whitetrasp = imagecolorallocatealpha($ghostImage, 255, 255, 255, 0);
-				imagefill($ghostImage, 0,0,$whitetrasp);
-				imagecopymerge($destImage, $ghostImage, 0,$destHeight,0,0,$destWidth, $destHeight, 50);
-				if($content_type == 'image/png')
-				{	//	problems mergins transparent png.. need to restore original pixel transparency
-					for ($x = 0; $x < $destWidth; $x ++)
-						for ($y = 0; $y < $destHeight; $y ++)
+				imagefill($ghostImage, 0, 0, $whitetrasp);
+				imagecopymerge($destImage, $ghostImage, 0, $destHeight, 0, 0, $destWidth, $destHeight, 50);
+				if ($content_type == 'image/png')
+				{ //	problems mergins transparent png.. need to restore original pixel transparency
+					for ($x = 0; $x < $destWidth; $x++)
+						for ($y = 0; $y < $destHeight; $y++)
 						{
 							$srcPixel = imagecolorsforindex($destImage, imagecolorat($destImage, $x, $y));
-							$destPixel = imagecolorsforindex($destImage, imagecolorat($destImage, $x, $y+$destHeight));
-							imagesetpixel($destImage, $x, $y+$destHeight, imagecolorallocatealpha($destImage,
-								$destPixel['red'], $destPixel['green'], $destPixel['blue'], $srcPixel['alpha']));
+							$destPixel = imagecolorsforindex($destImage, imagecolorat($destImage, $x, $y + $destHeight));
+							imagesetpixel($destImage, $x, $y + $destHeight, imagecolorallocatealpha($destImage, $destPixel['red'], $destPixel['green'], $destPixel['blue'], $srcPixel['alpha']));
 						}
-	
+
 				}
 				imagedestroy($ghostImage);
-	
+
 			}
-			
+
 			//save output to a buffer
 			ob_start();
-	
+
 			//create the image
 			switch ($content_type)
 			{
 				case 'image/gif' :
 					imagetruecolortopalette($destImage, true, 255);
 					//
-					if(imagecolortransparent($srcImage)>=0)
+					if (imagecolortransparent($srcImage) >= 0)
 					{
-						$maskImage = imagecreatetruecolor( $destWidth, $destHeight);
+						$maskImage = imagecreatetruecolor($destWidth, $destHeight);
 						imageantialias($maskImage, true);
 						imagealphablending($maskImage, false);
-						imagecopyresampled( $maskImage, $srcImage, 0, 0, 0, 0, $destWidth, $destHeight, $srcWidth, $srcHeight );
+						imagecopyresampled($maskImage, $srcImage, 0, 0, 0, 0, $destWidth, $destHeight, $srcWidth, $srcHeight);
 						//
-						$transp = imagecolorallocatealpha($destImage, 0,0,0,127);
+						$transp = imagecolorallocatealpha($destImage, 0, 0, 0, 127);
 						imagecolortransparent($destImage, $transp);
 						//
-						for ($x = 0; $x < $destWidth; $x ++)
-							for ($y = 0; $y < $destHeight; $y ++)
+						for ($x = 0; $x < $destWidth; $x++)
+							for ($y = 0; $y < $destHeight; $y++)
 							{
 								$c = imagecolorsforindex($maskImage, imagecolorat($maskImage, $x, $y));
-								if($c['alpha'] >= $alphaThreshold)
+								if ($c['alpha'] >= $alphaThreshold)
 								{
 									imagesetpixel($destImage, $x, $y, $transp);
-									if($appendGhosted)
-										imagesetpixel($destImage, $x, $y+$destHeight, $transp);
+									if ($appendGhosted)
+										imagesetpixel($destImage, $x, $y + $destHeight, $transp);
 								}
 							}
 						imagedestroy($maskImage);
@@ -277,64 +280,63 @@ class crpVideo
 					//
 					imagegif($destImage);
 					break;
-	
+
 				case 'image/jpeg' :
 				case 'image/pjpeg' :
 					imagejpeg($destImage);
 					break;
-	
+
 				case 'image/png' :
 					imagepng($destImage);
 					break;
 			}
-	
+
 			//copy output buffer to string
 			$resizedImage = ob_get_contents();
-	
+
 			//clear output buffer that was saved
 			ob_end_clean();
-	
+
 			//fre the memory used for the images
 			imagedestroy($srcImage);
 			imagedestroy($destImage);
-			
+
 			// credits to Mediashare by Jorn Lind-Nielsen
 			if (pnConfigGetVar('UseCompression') == 1)
-  			header("Content-Encoding: identity");
-  		
-  		// we need a timestamp
-  		$params['modifiedDate'] = DateUtil::parseUIDateTime($params['modifiedDate']);
-  			
-  		// Check cached versus modified date
-		  $lastModifiedDate = date('D, d M Y H:i:s T', $params['modifiedDate']);
-		  $currentETag = $params['modifiedDate'];
-		
-		  global $HTTP_SERVER_VARS;
-		  $cachedDate = (isset($HTTP_SERVER_VARS['HTTP_IF_MODIFIED_SINCE']) ? $HTTP_SERVER_VARS['HTTP_IF_MODIFIED_SINCE'] : null);
-		  $cachedETag = (isset($HTTP_SERVER_VARS['HTTP_IF_NONE_MATCH']) ? $HTTP_SERVER_VARS['HTTP_IF_NONE_MATCH'] : null);
-		
-		  // If magic quotes are on then all query/post variables are escaped - so strip slashes to make a compare possible
-		  // - only cachedETag is expected to contain quotes
-		  if (get_magic_quotes_gpc())
-		    $cachedETag = stripslashes($cachedETag);
-		
-		  if (    (empty($cachedDate) || $lastModifiedDate == $cachedDate)
-		      &&  '"'.$currentETag.'"' == $cachedETag)
-		  {
-		    header("HTTP/1.1 304 Not Modified");
-		    header("Status: 304 Not Modified");
-		    header("Expires: " . date('D, d M Y H:i:s T', time()+180*24*3600)); // My PHP insists on Expires in 1981 as default!
-		  	header('Pragma: cache'); // My PHP insists on putting a pragma "no-cache", so this is an attempt to avoid that
-		  	header('Cache-Control: public');
-		    header("ETag: \"$params[modifiedDate]\"");
-		    pnShutDown();
-		  }
-  			
-  		header("Expires: " . date('D, d M Y H:i:s T', time()+180*24*3600));
-  		header('Pragma: cache');
-  		header('Cache-Control: public');
-  		header("ETag: \"$params[modifiedDate]\"");
-	
+				header("Content-Encoding: identity");
+
+			// we need a timestamp
+			$params['modifiedDate'] = DateUtil :: parseUIDateTime($params['modifiedDate']);
+
+			// Check cached versus modified date
+			$lastModifiedDate = date('D, d M Y H:i:s T', $params['modifiedDate']);
+			$currentETag = $params['modifiedDate'];
+
+			global $HTTP_SERVER_VARS;
+			$cachedDate = (isset ($HTTP_SERVER_VARS['HTTP_IF_MODIFIED_SINCE']) ? $HTTP_SERVER_VARS['HTTP_IF_MODIFIED_SINCE'] : null);
+			$cachedETag = (isset ($HTTP_SERVER_VARS['HTTP_IF_NONE_MATCH']) ? $HTTP_SERVER_VARS['HTTP_IF_NONE_MATCH'] : null);
+
+			// If magic quotes are on then all query/post variables are escaped - so strip slashes to make a compare possible
+			// - only cachedETag is expected to contain quotes
+			if (get_magic_quotes_gpc())
+				$cachedETag = stripslashes($cachedETag);
+
+			if ((empty ($cachedDate) || $lastModifiedDate == $cachedDate) && '"' . $currentETag . '"' == $cachedETag)
+			{
+				header("HTTP/1.1 304 Not Modified");
+				header("Status: 304 Not Modified");
+				header("Expires: " . date('D, d M Y H:i:s T', time() + 180 * 24 * 3600)); // My PHP insists on Expires in 1981 as default!
+				header('Pragma: cache'); // My PHP insists on putting a pragma "no-cache", so this is an attempt to avoid that
+				header('Cache-Control: public');
+				header("ETag: \"$params[modifiedDate]\"");
+				pnShutDown();
+			}
+
+			header("Expires: " . date('D, d M Y H:i:s T', time() + 180 * 24 * 3600));
+			header('Pragma: cache');
+			header('Cache-Control: public');
+			header("ETag: \"$params[modifiedDate]\"");
+
 			header("Content-Disposition: inline; filename=thumb_{$filename}");
 			header("Content-Type: $content_type");
 			header("Last-Modified: $lastModifiedDate");
@@ -343,7 +345,6 @@ class crpVideo
 			pnShutDown();
 		}
 	}
-	
-}
 
+}
 ?>
