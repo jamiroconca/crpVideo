37f6f93ba162913dc0145b23e4938c7a4d8cbd64
jamiroconca <conca.daniele@gmail.com>
2008-06-19 16:21:23 +0000

    version upgrade

--- pnversion.php
+++ pnversion.php
@@ -2,24 +2,24 @@
 /**
  * crpVideo
  *
- * @copyright (c) 2007, Daniele Conca
- * @link http://noc.postnuke.com/projects/crpvideo Support and documentation
- * @author Daniele Conca <jami at cremonapalloza dot org>
- * @license GNU/GPL - v.2
+ * @copyright (c) 2007-2008, Daniele Conca
+ * @link http://code.zikula.org/projects/crpvideo Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
  * @package crpVideo
  */
 
 $modversion['name'] = _CRPVIDEO_NAME;
 $modversion['displayname'] = _CRPVIDEO_DISPLAYNAME;
 $modversion['description'] = _CRPVIDEO_DESCRIPTION;
-$modversion['version'] = '0.1.4';
+$modversion['version'] = '0.1.5';
 $modversion['credits'] = 'pndocs/credits.txt';
 $modversion['help'] = 'pndocs/install.txt';
 $modversion['changelog'] = 'pndocs/changelog.txt';
 $modversion['license'] = 'pndocs/license.txt';
 $modversion['official'] = 1;
 $modversion['author'] = 'Daniele Conca - jami';
-$modversion['contact'] = 'jami@cremonapalloza.org';
+$modversion['contact'] = 'conca.daniele@gmail.com';
 $modversion['securityschema'] = array ('crpVideo::Video' => 'Video title::Video ID');
 
 ?>
