a55268fa64a68d94b05abb3e2df68f739d4d3627
jamiroconca <conca.daniele@gmail.com>
2008-08-04 14:45:30 +0000

    added useful form list

--- modules/crpVideo/pnajax.php
+++ modules/crpVideo/pnajax.php
@@ -9,6 +9,37 @@
  * @package crpVideo
  */
 
+function crpVideo_ajax_getCategorizedVideo()
+{
+	if (!SecurityUtil::checkPermission('crpVideo::Video', '::', ACCESS_READ))
+	{
+		AjaxUtil :: error(pnVarPrepHTMLDisplay(_MODULENOAUTH));
+	}
+
+	pnModLangLoad('crpVideo', 'admin');
+	
+	// get all module vars
+	$modvars = pnModGetVar('crpVideo');
+	
+	// load the category registry util
+	if (!($class = Loader :: loadClass('CategoryRegistryUtil')))
+		pn_exit('Unable to load class [CategoryRegistryUtil] ...');
+	if (!($class = Loader :: loadClass('CategoryUtil')))
+		pn_exit('Unable to load class [CategoryUtil] ...');
+
+	$category = DataUtil::convertFromUTF8(FormUtil::getPassedValue('category', null, 'GET'));
+	$startnum = '1';
+	$mainCat = CategoryRegistryUtil :: getRegisteredModuleCategory('crpVideo', 'crpvideos', 'Main', '/__SYSTEM__/Modules/crpVideo');
+	$ignoreml = true;
+	$sortOrder = 'DESC';
+	$data = compact('startnum', 'category', 'clear', 'ignoreml', 'mainCat', 'cats', 'modvars', 'sortOrder');
+	
+	$videos = pnModAPIFunc('crpVideo', 'user', 'getall_formlist', $data);
+	
+	$resultlist = DataUtil::convertFromUTF8($videos);
+	return $resultlist;
+}
+
 function crpVideo_ajax_toggleStatus()
 {
 	if (!SecurityUtil::checkPermission('crpVideo::', '::', ACCESS_ADD))
